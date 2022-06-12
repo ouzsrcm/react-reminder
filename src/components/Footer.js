762fd9243577120df09765ba404647c4b9a8eb6a
ouzsrcm <oguzhan.saricam@icloud.com>
2022-06-12 14:37:37 +0300

    detais

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ const Footer = () => {
         <div className="container">
             <footer className="footer mt-auto py-3 bg-light">
                 <div className="container">
-                    <span className="text-muted">Place sticky footer content here.</span>
+                    <span className="text-muted">
+                        Pretty altlık :O :) :D
+                    </span>
                 </div>
             </footer>
         </div>
--- src/pages/ReminderDetails.js
+++ src/pages/ReminderDetails.js
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
+
+import { GetReminder } from '../services/Reminders';
+import {GetUserInfo} from '../services/User';
 
 function ReminderDetails(id){
     const {id: reminderId} = useParams();
+    const [reminder, setReminder] = useState(null);
+    const [user, setUser] = useState(null);
+    useEffect(()=>{
+        async function fetchData(){
+            const reminder = await GetReminder(reminderId);
+            setReminder(reminder);
+        }
+        fetchData();
+    }, []);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const user = await GetUserInfo();
+            setUser(user);
+        }
+        fetchData()
+    }, [])
     return(
         <>
             <h4>
-                {reminderId}
+                <small>#id:{reminderId}</small>
+                <p>
+                    {reminder?.title}
+                </p>
             </h4>
+            <div className='row'>
+                <div className='col-sm-6'>
+                    <ul className='list-group'>
+                    <li className='list-group-item'>user: {user?.name}</li> 
+                    <li className='list-group-item'>reminderid: {reminder?.id}</li> 
+                    <li className='list-group-item'>timezone: {reminder?.timezone}</li> 
+                    </ul>
+                </div>
+                <div className='col-sm-6'>
+                    asd
+                </div>
+            </div>
         </>
     )
 }
